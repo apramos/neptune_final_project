a6e2647b215027df865d5bfbdbb0c9c98bcf4ef3
patricia <ana-patricia.ramos@ens.lyon.fr>
2016-06-10 10:51:41 +0200

    added nuclt alignm

--- translate.py
+++ translate.py
@@ -6,38 +6,29 @@ import sys
 InFileName= 'seq_newname.fasta' 
 InFile= open(InFileName,'r')
 
-#OutFile=open('coding_frame.fasta','w')
+OutFile=open('coding_frame.fasta','w')
 
-for Sequence in InFile:
-	if not Sequence.startswith('>'):
+for Line in InFile:
+	if not Line.startswith('>'):
 		SearchString='(ATG)(\w\w\w)+?(TAA|TGA|TAG)'  
-		Results= list(re.finditer(SearchString, Sequence)) 
+		Results= list(re.finditer(SearchString, Line)) 
 		CodFrame =''
 		for Match in Results:
 			if len(Match.group(0))>len(CodFrame):
 				CodFrame=(Match.group (0))
-		print (CodFrame)
+		
+		#print (CodFrame)
+		OutFile.write(CodFrame+'\n')
 	else:
-		print (Sequence)
+		#print (Line)
+		OutFile.write (Line)
 
 
+InFile.close()
+OutFile.close()
 
-			#print(Match.group(0))
+		
 	
 
 
 
-
-
-#	if Result > 1000:
-#		print (Result)
-		#print (key)
-	"""	
-		NewHeader='>'+ AcessDict[key]+'\n'
-		NewHeader=NewHeader.replace(key,'')
-		print (NewHeader)
-		OutFile.write(NewHeader)
-	else:
-		print (Acession) 
-		OutFile.write(Acession)
-"""
